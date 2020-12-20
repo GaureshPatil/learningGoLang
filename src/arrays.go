146c17f298bb925c38bbe7c6f3d35f57a6be7028
GaureshPatil <gaureshp2@gmail.com>
2020-12-20 16:50:09 +0530

    use some functions present in array like length

---
+++ src/arrays.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var colors [3]string
+	colors[0] = "Red"
+	colors[1] = "Green"
+	colors[2] = "Blue"
+
+	fmt.Println(colors)
+
+	fmt.Println(colors[2])
+
+	var numbers = [5]int{5, 3, 1, 2, 4}
+	fmt.Println(numbers)
+
+	fmt.Println("number of colors:", len(colors))
+	fmt.Println("number of number array:", len(numbers))
+
+}
