9602e2521238efbe9e4ef934385bb5ed5fff7074
GaureshPatil <gaureshp2@gmail.com>
2020-12-20 17:03:44 +0530

    slices and cap function to check the size it works exactly like arrays but is more efficient and can perform more operations

--- src/slices.go
+++ src/slices.go
@@ -11,4 +11,24 @@ func main() {
 
 	colors = append(colors, "yellow")
 	fmt.Println(colors)
+
+	colors = append(colors[1:len(colors)])
+	fmt.Println(colors)
+
+	colors = append(colors[:len(colors)-1])
+	fmt.Println(colors)
+
+	number := make([]int, 5, 5)
+	number[0] = 1
+	number[1] = 7
+	number[2] = 32
+	number[3] = 12
+	number[4] = 156
+	fmt.Println(number)
+
+	number = append(number, 235)
+	fmt.Println(number)
+
+	fmt.Println(cap(number))
+
 }
