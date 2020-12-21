37cdb01980bfbc83f94983798c7d36498c0f682c
GaureshPatil <gaureshp2@gmail.com>
2020-12-21 15:38:53 +0530

    learned about return and multiple return via naked

---
+++ src/multireturn.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	n1, l1 := fullName("Gauresh", "Patil")
+	fmt.Printf("Full Name: %v, number of chars: %v\n\n", n1, l1)
+
+	n2, l2 := fullNameNakedReturn("Pritesh", "Patil")
+	fmt.Printf("Full Name: %v, number of chars: %v\n\n", n2, l2)
+
+}
+
+func fullName(f, l string) (string, int) {
+
+	full := f + " " + l
+	length := len(full)
+	return full, length
+}
+
+func fullNameNakedReturn(f, l string) (full string, length int) {
+
+	full = f + " " + l
+	length = len(full)
+	return
+}
