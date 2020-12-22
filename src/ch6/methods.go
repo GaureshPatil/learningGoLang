9ac8ffbd2f2e170b41f4ae579dbfe77d939e1af2
GaureshPatil <gaureshp2@gmail.com>
2020-12-22 12:01:23 +0530

    learned about methods in golang also learning about the concept of pointers

---
+++ src/ch6/methods.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Dog struct {
+	Breed string
+	Weight int
+	Sound string
+
+}
+
+func (d Dog) speak() {
+	fmt.Println(d.Sound)
+}
+
+func (d Dog) SpeakThreeTimes() {
+	d.Sound = fmt.Sprintf("%v! %v! %v!\n", d.Sound, d.Sound, d.Sound)
+	fmt.Print(d.Sound)
+}
+
+func main() {
+
+	poodle := Dog{"Poodle", 37,"Woof"}
+	fmt.Println(poodle)
+	poodle.speak()
+
+	poodle.Sound = "Arf"
+	poodle.speak()
+
+	poodle.SpeakThreeTimes()
+	poodle.SpeakThreeTimes()
+}
+
