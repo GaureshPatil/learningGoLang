61388377fe78e0ab58d9692bcc2f5963a93ecac1
GaureshPatil <gaureshp2@gmail.com>
2020-12-31 14:33:37 +0530

    learned about how to convert string format by creating new function

--- src/xml.go
+++ src/xml.go
@@ -4,15 +4,34 @@ import (
 	"fmt"
 	"net/http"
 	"io/ioutil"
+	"encoding/xml"
 )
+
+type SitemapIndex struct {
+	Locations []Location `xml:"sitemap"`
+}
+
+type Location struct {
+	Loc string `xml:"loc"`
+}
+
+// to format it in string 
+func (l Location) String() string {
+	return fmt.Sprintf(l.Loc)
+}
 
 func main() {
 
 	// underscore if you dont intend to use the variable and to ignore the error
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
-	string_body := string(bytes)
-	fmt.Println(string_body)
+	// string_body := string(bytes)
+	// fmt.Println(string_body)
 	resp.Body.Close()
+
+	var s SitemapIndex
+	xml.Unmarshal(bytes, &s)
+
+	fmt.Println(s.Locations)
 	
 }
